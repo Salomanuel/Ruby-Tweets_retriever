09340e85d78166ac68be00b2225c8457b7e44c53
Salomanuel <salomanuel.aux+code@gmail.com>
2018-02-10 18:03:32 +0100

    refactor in a more Object way

--- lib/twitter_mega.rb
+++ lib/twitter_mega.rb
@@ -3,7 +3,7 @@ require 'twitter'
 
 
 class TwitterMega
-	# configuration for the gem
+		# configuration for the gem
 	@@client = Twitter::REST::Client.new do |config|
 		config.consumer_key					=	"pjpFKcMYQpRKxHtlx7BOAl3VF"
 		config.consumer_secret			=	"0P97amDne0PGgL1AUK0pM9caHoeS8pK8ZpLHQUJptm6V0VVmdP"
@@ -14,56 +14,62 @@ class TwitterMega
 	def initialize
 		puts "this wonderful application will give you infinite knowledge \n(in the form of the latest tweets mentioning a certain user)\n"
 		
-		# all the choices are made here
-		menu
-
-		
-		print_results(mentionings(@user))
+			# all the user choices are made here, it's the interface
+		menu	
+			# main method, the workhorse
+		mentionings 			= mentionings(@user)
+			# sets how many results and removes retweets and own tweets
+		clean_mentionings = clean_up_mentionings(mentionings, @results_size)
+			# prints the results
+		print_results(clean_mentionings)
 	end
 
-	def menu
+
+
+		# all the choices are made here
+	def menu				
+			# choose user
 		puts "\nplease enter the user you are interested in (or press q to quit)"
 		puts "(blank is @esa)"
 		@user = gets.chomp
 		case @user
 		when "q" then exit
-		when ""  then @user = "esa"				# default user is @esa
+				# default user is @esa
+		when ""  then @user = "esa"				
 		end
-
+			# choose number or results
 		puts "select how many results you want"
 		puts "please keep it low or I will get banned from the free Twitter API"
-		@results_size = gets.chomp
-		while @results_size.to_i > 50
+		@results_size = gets.chomp.to_i
+		while @results_size > 50
 			puts "\ncome on, choose a smaller number please"
-			@results_size = gets.chomp
+			@results_size = gets.chomp.to_i
 		end
 	end
 	
-	# this method is not used, it was just to check the API
-	# RSpec is still using it to make sure the app can talk to the API
+		# this method is not used, it was just to check the API
+		# RSpec is still using it to make sure the app can talk to the API
 	def tweet_fetch(user)
 		@@client.user_timeline(user)
 	end
 
 
-	# main method
+		# MAIN SAIL method
 	def mentionings(user)
 		puts "asking twitter for mentionings of @#{user}...it may take a while"
 
 		# this returns a potentially VERY long result list that also has the user's own tweets
 		# please don't use EACH on it or it will return VERY long results
-		mentionings = @@client.search("@#{user}") # in a far future, the options hash may be necessary    #, options = { count: 100 })
-		
-		# this removes the retweets
-		clean_up_mentionings(mentionings)
+		@@client.search("@#{user}") # in a far future, the options hash may be necessary    #, options = { count: 100 })
 	end
 
-	def clean_up_mentionings(mentionings)
+		# sets the results quantity and removes retweets and own tweets
+	def clean_up_mentionings(mentionings, length)
 		cleaned_mentionings = []
 		mentionings.each do |mentioning|
-			break if cleaned_mentionings.length > 10
-			next if mentioning.text[0..6] == "RT @esa"	# removes retweets
-			next if mentioning.text[0..3] == "@esa"			# removes own tweets
+			break if cleaned_mentionings.length > length # decides length
+			next if mentioning.text[0..6] == "RT @esa"	 # removes retweets
+			next if mentioning.text[0..3] == "@esa"			 # removes own tweets
 			cleaned_mentionings << mentioning 
 		end
 		return cleaned_mentionings
@@ -79,15 +85,17 @@ end
 
 TwitterMega.new
 
+
+
+
+# DEBUG things, not necessary for the operation of the app
+
 # TwitterMega.new.mentionings("esa").each_with_index do |result, i|
 # 	puts "#{i}"
 # 	puts result.text
 	 
 # end
 
-
-
-# DEBUG things, not necessary for the app
 # twit = TwitterMega.new
 # puts twit.tweet_fetch("salomanuel").first.text
 
