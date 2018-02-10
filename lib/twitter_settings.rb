f5de3a3695a08c266715c924d2dbb74b98336f8e
Salomanuel <salomanuel.aux+code@gmail.com>
2018-02-10 21:53:06 +0100

    settings can be saved on a file

--- lib/twitter_settings.rb
+++ lib/twitter_settings.rb
@@ -1,13 +1,7 @@
-require 'twitter'
-
 module TwitterConfig
-
-	# if File.exists?('lib/keys.dat')
-	# 	puts "read from file"
-	# else
-	# 	puts "create file"
-	# 	settings = File.new('lib/keys.dat', 'w')
-	# end
+	require 'twitter'
+	# require 'yaml'
+	require 'json'
 
 	def explanations
 		puts "Twitter API v1.1 requires you to authenticate via OAuth, so you'll need to register your application with Twitter. Once you've registered an application, make sure to set the correct access level, otherwise you may see the error:"
@@ -31,6 +25,20 @@ module TwitterConfig
 		@access_secret 		= gets.chomp
 	end
 
+	def write_config
+		# file = YAML::load_file('lib/keys.dat') || {}
+		# self.consumer_key = "a"
+		# File.open('lib/keys.dat', 'r+') do |file|
+		# 	file.write(file.to_yaml)
+		# end
+		File.open("keys.dat", "w") do |f|
+			f.write(consumer_key: 			 @consumer_key,
+							consumer_secret: 		 @consumer_secret,
+							access_token:				 @access_token,
+							access_token_secret: @access_secret)
+		end
+	end
+
 
 
 
@@ -46,8 +54,14 @@ end
 class Uoo
 	include TwitterConfig
 	def initialize
-		explanations
+		if File.exists?('keys.dat')
+			puts "loading settings"
+		else
+			explanations 
+			settings = File.new('keys.dat', 'w')
+		end
 		get_config
+		write_config
 	end
 end
 
