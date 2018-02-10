af3ba7aae52472aa0d50ec8177c611e1710603d7
Salomanuel <salomanuel.aux+code@gmail.com>
2018-02-10 14:46:20 +0100

    the app seems to retrieve tweets correctly

--- lib/twitter_mega.rb
+++ lib/twitter_mega.rb
@@ -4,14 +4,22 @@ require 'twitter'
 
 class TwitterMega
 	# configuration for the gem
-	client = Twitter::REST::Client.new do |config|
+	@@client = Twitter::REST::Client.new do |config|
 		config.consumer_key					=	"pjpFKcMYQpRKxHtlx7BOAl3VF"
 		config.consumer_secret			=	"0P97amDne0PGgL1AUK0pM9caHoeS8pK8ZpLHQUJptm6V0VVmdP"
 		config.access_token					=	"770927087934996480-o7yhhEBsBPi6FNDGdVKhIHBOkAgLmlb"
 		config.access_token_secret	= "FsE2XlnwOorLMQp67PPdqm2behxmLS0SR5PqmKlpNrHn1"
 	end
-
+	def tweet_fetch(user)
+		@@client.user_timeline(user)
+	end
 end
+
+
+
+# DEBUG things, not necessary for the app
+# twit = TwitterMega.new
+# puts twit.tweet_fetch("salomanuel").first.text
 
 # client.user_timeline("esa").each do |tweet|
 # 	puts tweet.text
