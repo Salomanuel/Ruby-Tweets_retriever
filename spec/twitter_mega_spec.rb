c6557303dcb91284ce50e89ebba8251d43255843
Salomanuel <salomanuel.aux+code@gmail.com>
2018-02-10 14:45:58 +0100

    slightly update my last tweet

--- spec/twitter_mega_spec.rb
+++ spec/twitter_mega_spec.rb
@@ -3,8 +3,8 @@ require "twitter_mega"
 describe TwitterMega do
 	describe ".tweet_fetch" do
 		it "gets the first tweet from my old account" do
-			my_last_tweet = "Ascent successful. Dragon enroute to Space Station. Rocket landed on droneship, but too hard for survival."
-			expect(TwitterMega.tweet_fetch("salomanuel").first).to eql(my_last_tweet)
+			my_last_tweet = "RT @elonmusk: Ascent successful. Dragon enroute to Space Station. Rocket landed on droneship, but too hard for survival."
+			expect(TwitterMega.new.tweet_fetch("salomanuel").first.text).to eql(my_last_tweet)
 		end
 	end
 end
